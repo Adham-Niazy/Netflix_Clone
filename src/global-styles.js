ab997044696ae311502613d85932e639dcf18030
Adham-Niazy <adhamniazy@gmail.com>
2020-10-19 15:40:58 +0200

    Adding file contain Global-Styling

---
+++ src/global-styles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-family: 'Nunito', sans-serif;
+        background-color: black;
+        color: #333;
+        font-size: 16px;
+    }
+`;
