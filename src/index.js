03fc87f6c10bd331ada8fb19a45eab9a7a283373
Adham-Niazy <adhamniazy@gmail.com>
2020-10-19 15:42:33 +0200

    Updating index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import { firebase } from './lib/firebase.prod';
+import 'normalize.css';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+import App from './app';
+import { GlobalStyles } from './global-styles';
+import { FirebaseContext } from './context/firebase';
+
+ReactDOM.render(
+    <>
+        <FirebaseContext.Provider value={{ firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
+    </>,
+    document.getElementById('root'));
