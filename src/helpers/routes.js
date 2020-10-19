198117a47fc0460f58dd0db21da7240aa4217a24
Adham-Niazy <adhamniazy@gmail.com>
2020-10-19 15:49:14 +0200

    Adding Two helpers fuctions to Check Auth and improve UI Redirecting

---
+++ src/helpers/routes.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export const IsUserRedirect = ({ user, loggedInPath, children, ...restProps }) => {
+    return (
+        <Route 
+            {...restProps}
+            render={ () => {
+                if (!user) {
+                    return children;
+                }
+
+                if (user) {
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: loggedInPath
+                            }}
+                        />
+                    );
+                }
+
+                return null;
+            }}
+        />
+    );
+};
+
+export const ProtectedRoute = ({ user, children, ...restProps }) => {
+    return (
+        <Route
+            {...restProps}
+            render={({ location }) => {
+                if (user) {
+                    return children;
+                }
+
+                if (!user) {
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: 'signin',
+                                state: { from: location }
+                            }}
+                        />
+                    )
+                }
+
+                return null;
+            }}
+        />
+    );
+};
