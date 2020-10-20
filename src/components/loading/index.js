98b6dd2447eb9e820a76e53f4d2b7405ff9ba4d2
Adham-Niazy <adhamniazy@gmail.com>
2020-10-20 03:55:41 +0200

    Adding Loading Spinner Component

---
+++ src/components/loading/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { LockBody, ReleaseBody, Spinner, Picture } from './styles/loading';
+
+const Loading = ({ src, ...restProps }) => {
+  console.log(src);
+  return (
+    <Spinner {...restProps}>
+      <LockBody />
+      <Picture src={`/images/users/${src}.png`} data-testid="loading-picture" />
+    </Spinner>
+  );
+}
+
+Loading.ReleaseBody = function LoadingReleaseBody() {
+  return <ReleaseBody />;
+};
+
+export default Loading;
