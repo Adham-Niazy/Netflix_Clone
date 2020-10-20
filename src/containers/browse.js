08824a7e1853a15c843020b499cbaefc8a9b4a1c
Adham-Niazy <adhamniazy@gmail.com>
2020-10-20 22:31:44 +0200

    Adding the live search Logic using Fuse.js

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import Fuse from 'fuse.js';
 
 import SelectProfileContainer from './profiles';
 import { FirebaseContext } from '../context/firebase';
@@ -26,6 +27,22 @@ const BrowseContainer = ({ slides }) => {
     useEffect(() => {
         setSlideRows(slides[category]);
     }, [slides, category]);
+
+
+    // Using Fuse to do search filteration
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, {
+            keys: ['data.description', 'data.title', 'data.genre']
+        });
+
+        const results = fuse.search(searchTerm).map( ({item}) => item );
+
+        if ( slideRows.length > 0 && searchTerm.length > 3 && results.length > 0 ) {
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+    }, [searchTerm]);
 
     return profile.displayName ? ( 
         <>
